14dedb3055f1a4aebbe257aa1e3a97c7719c6986
Jess on the Hope <jesshon.py326@gmail.com>
2022-03-02 10:08:18 -0500

    camera permission
    added request camera permission at runtime

--- app/src/main/java/com/bitpunchlab/android/topdf/intro/IntroFragment.kt
+++ app/src/main/java/com/bitpunchlab/android/topdf/intro/IntroFragment.kt
@@ -1,19 +1,59 @@
 package com.bitpunchlab.android.topdf.intro
 
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.MutableLiveData
 import androidx.navigation.fragment.findNavController
 import com.bitpunchlab.android.topdf.R
 import com.bitpunchlab.android.topdf.databinding.FragmentIntroBinding
 
+private const val TAG = "IntroFragment"
+
+// we check the camera permission here, if the permission is not granted,
+// we request it
 class IntroFragment : Fragment() {
 
     private var _binding: FragmentIntroBinding? = null
 
     private val binding get() = _binding!!
+
+    private var cameraEnabled = MutableLiveData<Boolean>(false)
+/*
+    private val requestCameraPermissionResult =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+                result: ActivityResult ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                Log.i(TAG, "camera permission granted")
+                cameraEnabled.value = true
+            } else {
+                Log.i(TAG, "camera permission not granted")
+                // show an alert
+            }
+        }
+*/
+
+    private val requestCameraPermissionResult =
+        registerForActivityResult(ActivityResultContracts.RequestPermission())
+        { isGranted: Boolean ->
+            if (isGranted) {
+                Log.i(TAG, "camera permission granted")
+                findNavController().navigate(R.id.action_IntroFragment_to_createJobFragment)
+            } else {
+                Log.i(TAG, "camera permission not granted")
+            }
+        }
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
@@ -23,8 +63,13 @@ class IntroFragment : Fragment() {
         _binding = FragmentIntroBinding.inflate(inflater, container, false)
 
         binding.createJobButton.setOnClickListener {
-            // navigate to create a job fragment
-            findNavController().navigate(R.id.action_IntroFragment_to_createJobFragment)
+            if (!checkCameraPermission()) {
+                //val enableCameraAccessIntent = Intent(Manifest.permission.CAMERA)
+                requestCameraPermissionResult.launch(Manifest.permission.CAMERA)
+            } else {
+                // navigate to create a job fragment
+                findNavController().navigate(R.id.action_IntroFragment_to_createJobFragment)
+            }
         }
 
         binding.displayJobButton.setOnClickListener {
@@ -42,13 +87,15 @@ class IntroFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        //binding.buttonFirst.setOnClickListener {
-        //    findNavController().navigate(R.id.action_IntroFragment_to_MainFragment)
-        //}
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun checkCameraPermission() : Boolean {
+        return ContextCompat.checkSelfPermission(requireContext(),
+            android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED
     }
 }
--- app/src/main/java/com/bitpunchlab/android/topdf/main/MainFragment.kt
+++ app/src/main/java/com/bitpunchlab/android/topdf/main/MainFragment.kt
@@ -1,20 +1,43 @@
 package com.bitpunchlab.android.topdf.main
 
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.MutableLiveData
 import androidx.navigation.fragment.findNavController
 import com.bitpunchlab.android.topdf.R
 import com.bitpunchlab.android.topdf.databinding.FragmentMainBinding
 
+private const val TAG = "MainFragment"
+private const val REQUEST_CAMERA_PERMISSION = 203
+
+// all the main functions of the app are mostly here
 
 class MainFragment : Fragment() {
 
     private var _binding: FragmentMainBinding? = null
 
     private val binding get() = _binding!!
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // make sure the app can use camera
+        //checkCameraPermission()
+
+    }
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
@@ -40,4 +63,6 @@ class MainFragment : Fragment() {
         super.onDestroyView()
         _binding = null
     }
+
+
 }
